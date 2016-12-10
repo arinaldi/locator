c5744c4c5e76c2798d6b07bc4a0c8d151913df47
Tony <arinaldi@vt.edu>
2016-12-10 16:48:00 -0600

    Add REST API

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
+//forum
+//https://forums.manning.com/forums/getting-mean-with-mongo-express-angular-and-node
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-require('./app_server/models/db');
+require('./app_api/models/db');
 
 //var index = require('./routes/index');
 var routes = require('./app_server/routes/index');
-var users = require('./app_server/routes/users');
+var routesApi = require('./app_api/routes/index');
+//var users = require('./app_server/routes/users');
 
 var app = express();
 
@@ -25,7 +29,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/api', routesApi);
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
